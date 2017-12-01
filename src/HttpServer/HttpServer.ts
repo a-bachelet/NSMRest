71d14f347c8f681bce8fdef9c69aca4cfbcf6b16
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-01 22:34:07 +0100

    Initialization added to HttpServer

--- src/HttpServer/HttpServer.ts
+++ src/HttpServer/HttpServer.ts
@@ -11,6 +11,9 @@ export default class HttpServer {
     private _express: express.Application;
     private _expressPort: number;
 
+    private _apiRouter: express.Router;
+    private _webRouter: express.Router;
+
     constructor(expressPort: number) {
         this._console = new Console();
         this._express = express();
@@ -19,9 +22,35 @@ export default class HttpServer {
 
     public start(): void {
         this._console.success('| (s) Welcome On NodeServerMonitor !' + '\n');
+        this.init();
         this._express.listen(this._expressPort, () => {
             this._console.success('| (s) Server is listening on port -> ' + this._expressPort);
         });
+    }
+
+    private initRouters(): void {
+        this._apiRouter = express.Router();
+        this._webRouter = express.Router();
+        this._express.use('/api', this._apiRouter);
+        this._express.use('/web', this._webRouter);
+    }
+
+    private initControllers(): void {
+        this._console.action('|      -> Controllers initialization...');
+        this._console.action('|      -> Controllers initialized !');
+    }
+
+    private initMiddlewares(): void {
+        this._console.action('|      -> Middlewares initialization...');
+        this._console.action('|      -> Middlewares initialized !');
+    }
+
+    private init(): void {
+        this._console.info('| (i) Initialization started...');
+        this.initRouters();
+        this.initControllers();
+        this.initMiddlewares();
+        this._console.success('| (s) Server successfully initialized !' + '\n');
     }
 
 }
