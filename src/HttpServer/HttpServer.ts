e75c25eee465615c06cd0073b3fb0dd0c5a9bbc7
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-01 22:50:46 +0100

    Added routers intialization logging information

--- src/HttpServer/HttpServer.ts
+++ src/HttpServer/HttpServer.ts
@@ -2,7 +2,10 @@
 import Console from '../Console/Console';
 
 /** Dependencies Imports */
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
 export default class HttpServer {
 
@@ -29,24 +32,35 @@ export default class HttpServer {
     }
 
     private initRouters(): void {
+        this._console.action('|      -> Routers initialization...');
         this._apiRouter = express.Router();
         this._webRouter = express.Router();
         this._express.use('/api', this._apiRouter);
+        this._console.success('|            -> API Router loaded !');
         this._express.use('/web', this._webRouter);
+        this._console.success('|            -> WEB Router loaded !');
+        this._console.action('|      -> Routers initialized !' + '\n');
     }
 
     private initControllers(): void {
         this._console.action('|      -> Controllers initialization...');
-        this._console.action('|      -> Controllers initialized !');
+        this._console.action('|      -> Controllers initialized !' + '\n');
     }
 
     private initMiddlewares(): void {
         this._console.action('|      -> Middlewares initialization...');
-        this._console.action('|      -> Middlewares initialized !');
+        this._express.use(bodyParser.urlencoded({ extended: true }));
+        this._express.use(bodyParser.json());
+        this._console.success('|         -> Body Parser Middleware successfully loaded !');
+        this._express.use(helmet());
+        this._console.success('|         -> Helmet Middleware succesfully loaded !');
+        this._express.use(morgan('dev'));
+        this._console.success('|         -> Morgan Middleware succesfully loaded !');
+        this._console.action('|      -> Middlewares initialized !' + '\n');
     }
 
     private init(): void {
-        this._console.info('| (i) Initialization started...');
+        this._console.info('| (i) Initialization started...' + '\n');
         this.initRouters();
         this.initControllers();
         this.initMiddlewares();
