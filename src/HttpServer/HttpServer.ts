29de663ab5a16a8baeb4889ed861ac1462f6e77e
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-01 22:33:26 +0100

    Console used in HttpServer to beautify logging

--- src/HttpServer/HttpServer.ts
+++ src/HttpServer/HttpServer.ts
@@ -1,20 +1,26 @@
+/** Classes Imports */
+import Console from '../Console/Console';
+
 /** Dependencies Imports */
 import * as express from 'express';
 
 export default class HttpServer {
 
+    private _console: Console;
+
     private _express: express.Application;
     private _expressPort: number;
 
     constructor(expressPort: number) {
+        this._console = new Console();
         this._express = express();
         this._expressPort = expressPort;
     }
 
     public start(): void {
+        this._console.success('| (s) Welcome On NodeServerMonitor !' + '\n');
         this._express.listen(this._expressPort, () => {
-            console.log('| Welcome On NodeServerMonitor !');
-            console.log('| Server is listening on port -> ' + this._expressPort);
+            this._console.success('| (s) Server is listening on port -> ' + this._expressPort);
         });
     }
 
