23b30601e846365688a26300d39a549d3d8055b8
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-03 21:23:56 +0100

    Added databaseUrl var in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@
 import HttpServer from './HttpServer/HttpServer';
 
 /** Load Env Vars */
-const expressPort: number = Number(process.env.EXPRESS_PORT) || 3000;
+const databaseUrl: string = 'mongodb://localhost/NodeServerMonitor' ||  process.env.DATABASE_URL;
+const httpPort: number = Number(process.env.HTTP_PORT) || 3000;
 
 /** Instanciation of HttpServer */
-const httpServer: HttpServer = new HttpServer(expressPort);
+const httpServer: HttpServer = new HttpServer(databaseUrl, httpPort);
 
 /** Start HttpServer */
 httpServer.start();
