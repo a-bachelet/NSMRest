a1dc5ea63879b68218e3dc65e518eb64827ac41c
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-27 00:52:58 +0100

    RoleController added to CONTROLLERS constant

--- src/Constants/CONTROLLERS.ts
+++ src/Constants/CONTROLLERS.ts
@@ -3,7 +3,9 @@ import IController from '../Interfaces/IController';
 
 /** Classes Imports */
 import DefautltController from '../Controllers/DefaultController';
+import RoleController from '../Controllers/RoleController';
 
 export const CONTROLLERS: IController[] = [
-    new DefautltController()
+    new DefautltController(),
+    new RoleController()
 ];
