3795ec191554dcf2d87464114539a1394a86eef6
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-27 18:48:04 +0100

    SystemController class created

---
+++ src/Controllers/SystemController.ts
@@ -0,0 +1,68 @@
+/** Dependencies Imports */
+import * as diskinfo from 'diskinfo';
+import * as express from 'express';
+import * as os from 'os';
+
+/** Interfaces Imports */
+import IController from '../Interfaces/IController';
+import IRoute from '../Interfaces/IRoute';
+
+export default class SystemController implements IController {
+
+    public controllerPath: string = '/system';
+
+    private routes: IRoute[] = [
+        { method: 'GET', path: '/common', callable: this.getCommon },
+        { method: 'GET', path: '/cpus', callable: this.getCpus },
+        { method: 'GET', path: '/disks', callable: this.getDisks },
+        { method: 'GET', path: '/networks', callable: this.getNetworks }
+    ];
+
+    public getRoutes(): IRoute[] {
+        return this.routes;
+    }
+
+    private getCommon(req: express.Request, res: express.Response): void {
+        const arch = os.arch();
+        const hostname = os.hostname();
+        const platform = os.platform();
+        const release = os.release();
+        const type = os.type();
+        const uptime = Math.floor(os.uptime());
+        res.status(200).send({
+            success: true,
+            arch,
+            hostname,
+            platform,
+            release,
+            type,
+            uptime
+        });
+    }
+
+    private getCpus(req: express.Request, res: express.Response): void {
+        const cpus = os.cpus();
+        res.status(200).send({
+            success: true,
+            cpus
+        });
+    }
+
+    private getDisks(req: express.Request, res: express.Response): void {
+        diskinfo.getDrives((err: any, disks: any[]) => {
+            res.status(200).send({
+                success: true,
+                disks
+            });
+        });
+    }
+
+    private getNetworks(req: express.Request, res: express.Response): void {
+        const networks = os.networkInterfaces();
+        res.status(200).send({
+            success: true,
+            networks
+        });
+    }
+
+}
