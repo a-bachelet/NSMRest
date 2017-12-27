a2601af6369bcd233e40ba1c3d807157a9517887
Alexis Bachelet <app.bachelet@gmail.com>
2017-12-27 13:33:32 +0100

    AuthController addes to CONTROLLERS constant

--- src/Constants/CONTROLLERS.ts
+++ src/Constants/CONTROLLERS.ts
@@ -2,11 +2,13 @@
 import IController from '../Interfaces/IController';
 
 /** Classes Imports */
+import AuthController from '../Controllers/AuthController';
 import DefautltController from '../Controllers/DefaultController';
 import RoleController from '../Controllers/RoleController';
 import UserController from '../Controllers/UserController';
 
 export const CONTROLLERS: IController[] = [
+    new AuthController(),
     new DefautltController(),
     new RoleController(),
     new UserController()
